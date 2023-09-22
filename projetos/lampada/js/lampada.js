e24e3bfbd7167b3b7bdefb44f237851e8be2fd05
HasteXYZ <caio.csc@hotmail.com.br>
2023-09-22 13:23:22 -0300

    22/09

--- projetos/lampada/js/lampada.js
+++ projetos/lampada/js/lampada.js
@@ -1,45 +1,47 @@
-//Aqui são as minhas funções , ou seja meus métodos aquelas ações que eu gostaria que meu site tivesse. 
-const turnOn = document. getElementById ('turnOn');
-const turnOff = document. getElementById ('turnOff');
-const lamp = document.getElementById ('lamp');
 
-var on = "./img/off.png";
-var off = "./img/on.png";
 
-function isLampBroken () {
-    return lamp.src.indexOf ( 'quebrada' ) > -1
+const turnOn = document.getElementById('turnOn');
+const turnOff = document.getElementById('turnOff');
+const lamp = document.getElementById('lamp');
+const rosa = document.getElementById('turnrosa');
+
+function isLampBroken() {
+    return lamp.src.indexOf('quebrada') > -1;
 }
 
-//aqui você faz acontecer
-function lampOn (){
-    if(!isLampBroken ()){
-    lamp.src = './img/ligada.png';
-    turnOn.src = './on/on.png'
+function lampOn() {
+    if (!isLampBroken()) {
+        lamp.src = './img/ligada.png';
+        turnOn.src = './on/on.png';
+        turnOff.src = './off/off.png'; // Atualiza a imagem do botão de desligar.
     }
-
 }
 
-function lampOff (){
-    if(!isLampBroken ()){
+function lampOff() {
+    if (!isLampBroken()) {
         lamp.src = './img/desligada.png';
         turnOff.src = './off/off.png';
-        }
+        turnOn.src = './on/on.png'; // Atualiza a imagem do botão de ligar.
+    }
 }
 
-function lampBroken (){
+function lampRosa() {
+    if (!isLampBroken()) {
+        lamp.src = './img/coz.rosa.png';
+        turnOn.src = './on/on.png';
+        turnrosa.src = './coz.rosa/coz.rosa.png'; // Atualiza a imagem do botão de ligar.
+    }
+}
+
+function lampBroken() {
     lamp.src = './img/quebrada.png';
 }
 
+turnOn.addEventListener('click', lampOn);
+turnOff.addEventListener('click', lampOff);
+turnrosa.addEventListener('click', lampRosa);
 
-
-//aqui você tem um evento
-turnOn.addEventListener ( 'click', lampOn );
-turnOff.addEventListener ( 'click', lampOff );
-
-//ao passar o mouse sobre a imagem ela vai ligar e desligar, para assim conseguirmos trocar a imagem 
-//para uma quebrada. 
-lamp.addEventListener ( 'mouseover', lampOn );
-//ao sair da imagem ela volta a anterior 
-lamp.addEventListener ( 'mouseleave', lampOff );
-lamp.addEventListener ( 'dblclick', lampBroken );
-
+lamp.addEventListener('mouseover', lampOn);
+lamp.addEventListener('mouseleave', lampOff);
+lamp.addEventListener('mouseover', lampRosa);
+lamp.addEventListener('dblclick', lampBroken);
